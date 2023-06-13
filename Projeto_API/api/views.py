f7331cbfae362df47bc8f8c15f8c436836baec54
Marcelo Galli <marcelolemesgalli@hotmail.com>
2023-06-13 14:35:46 -0300

    update
    update

--- Projeto_API/api/views.py
+++ Projeto_API/api/views.py
@@ -7,7 +7,7 @@ class CompanyView(APIView):
   def get(self, request, format=None):
     companies=Company.objects.all()
     if len(companies)>0:
-      
+      data={'message': 'Companies not found...'}
   
   def post(self, request, format=None):
     pass
