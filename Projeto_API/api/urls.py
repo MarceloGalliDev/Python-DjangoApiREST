f0b507be4c3ef3e5a69f80d0e5aa46842c4c47d2
Marcelo Galli <marcelolemesgalli@hotmail.com>
2023-06-13 15:40:12 -0300

    update
    update

--- Projeto_API/api/urls.py
+++ Projeto_API/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import CompanyView
+from .views.company_view import CompanyView
 
 urlpatterns = [
-  path('companies/', CompanyView.as_views(), name='companies_list')
+  path('companies/', CompanyView.as_view(), name='companies_list')
 ]
--- Projeto_API/api/views/company_view.py
+++ Projeto_API/api/views/company_view.py
@@ -1,13 +1,17 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status as status_http
 from django.http.response import JsonResponse
 from django.shortcuts import render
-from rest_framework.views import APIView
-from .models import Company
+from ..models import Company
+from django.views import View
+from ..serializers import company_serializer
 
-class CompanyView(APIView):
+class CompanyView(View):
   
-  def get(self, request, format=None):
-    companies=Company.objects.all()
-    if len(companies)>0:
+  def get(self, request):
+    companies = list(Company.objects.values())
+    if len(companies) > 0:
       data = {'message': 'Sucess', 'companies':companies}
     else:
       data = {'message': 'Companies not found...'}
